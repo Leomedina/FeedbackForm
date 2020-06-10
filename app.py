40294fe99e0d764c0ed70f8ead08f52c9723d4d2
Leo <medinayleo@gmail.com>
2020-06-10 18:36:18 -0400

    authenticating users

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User
+from forms import LoginForm, SignUpForm
 
 app = Flask(__name__)
 
@@ -14,4 +15,51 @@ debug = DebugToolbarExtension(app)
 
 connect_db(app)
 
-###ROUTES COMING###
+@app.route('/')
+def show_home():
+    return redirect('/register')
+
+@app.route('/users/<username>')
+def show_userpage(username):
+    if "username" not in session:
+        flash("Please login to see the secret")
+        return redirect('/login')
+    u = User.query.filter_by(username = username).first()
+    return render_template('/user.html', user = u)
+
+@app.route('/register', methods = ["GET", "POST"])
+def register_user():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        new_user = User.register(username, password, email, first_name, last_name)
+        flash('Success! Created new account')
+        db.session.add(new_user)
+        db.session.commit()
+        session['username'] = username
+        return redirect(f'/users/{username}')
+    return render_template('/signup.html', form = form)
+
+@app.route('/login', methods = ["GET", "POST"])
+def authenticate_user():
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        if User.authenticate(username, password):
+            flash('LOGIN Successful')
+            session['username'] = username
+            return redirect(f'/users/{username}')
+        else: 
+            form.username.errors = ["Invalid Username/Password"]
+    return render_template('/login.html', form = form)
+
+@app.route('/logout')
+def logout_user():
+    session.pop('username')
+    flash("Goodbye!")
+    return redirect('/')
---
+++ forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Length, NumberRange, Optional
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[InputRequired(), Length(min=1, max=20)])
+    password = PasswordField("Password", validators=[InputRequired(), Length(min=5, max=50)])
+
+class SignUpForm(FlaskForm):
+    username = StringField("Username", validators=[InputRequired(), Length(min=1, max=20)])
+    password = PasswordField("Password", validators=[InputRequired(), Length(min=5, max=50)])
+    email = StringField("Email", validators=[InputRequired(), Length(max=50)])
+    first_name = StringField("First Name", validators=[InputRequired(), Length(max=30)])
+    last_name = StringField("Last Name", validators=[InputRequired(), Length(max=30)])
